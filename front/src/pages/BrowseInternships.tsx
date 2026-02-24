c0d333711dd2b7a67fde7c42a8f0f6195506847e
Seif Badreldin <seifmohamed3535@gmail.com>
2026-02-24 21:38:04 +0200

    fix: add id and name to search input for accessibility

--- front/src/pages/BrowseInternships.tsx
+++ front/src/pages/BrowseInternships.tsx
@@ -110,6 +110,8 @@ export function BrowseInternships() {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
                 <input
                   type="text"
+                  id="internship-search"
+                  name="internship_search"
                   placeholder="Search by title or company..."
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
